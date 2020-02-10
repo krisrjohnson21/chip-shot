d81442ac96d9b3eb793cb9bd8479f2e15747df40
Kris Johnson <krisrjohnson@gmail.com>
2020-02-10 18:59:49 -0500

    adjusted index and app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,14 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first, :last, :city, :state, :handicap, :bio])
+    devise_parameter_sanitizer.permit(:sign_up,
+      keys: [
+        :first,
+        :last,
+        :city,
+        :state,
+        :handicap,
+        :bio
+      ])
   end
 end
