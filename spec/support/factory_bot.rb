f72c7311eae43ae75c96f65aaf8e9ab3fe8d48d6
Kris Johnson <krisrjohnson@gmail.com>
2020-02-10 14:20:54 -0500

    Updated factorybot field definitions

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -2,6 +2,12 @@ require 'factory_bot'
 
 FactoryBot.define do
   factory :user do
+    sequence(:first) {|n| "John" }
+    sequence(:last) {|n| "Smith" }
+    sequence(:city) {|n| "Tucson" }
+    sequence(:state) {|n| "Arizona" }
+    sequence(:handicap) {|n| "25" }
+    sequence(:bio) {|n| "I love winter golf!" }
     sequence(:email) {|n| "user#{n}@example.com" }
     password { 'password' }
     password_confirmation { 'password' }
