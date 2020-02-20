b64e9f76140bac150f486f7220ae019a577f6568
Kris Johnson <krisrjohnson@gmail.com>
2020-02-20 09:38:35 -0500

    Updated controller tests

--- spec/controllers/api/v1/rounds_controller_spec.rb
+++ spec/controllers/api/v1/rounds_controller_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe Api::V1::UsersController, type: :controller do
+RSpec.describe Api::V1::RoundsController, type: :controller do
   let!(:first_user) { User.create(
     id: 1,
     first: "Timothy",
@@ -31,32 +31,33 @@ RSpec.describe Api::V1::UsersController, type: :controller do
     user: first_user,
   ).to_json }
 
-  describe "GET#show" do
-    it "should return the user and their details" do
-      get :show, params: {id: first_user.id}
+  describe "GET#index" do
+    it "should return the user's rounds" do
+      get :index, params: {
+        user_id: first_user.id,
+        rounds: first_user.rounds
+      }
+      
       returned_json = JSON.parse(response.body)
 
       expect(response.status).to eq 200
       expect(response.content_type).to eq("application/json")
 
-      expect(returned_json.length).to be 1
+      expect(returned_json.length).to be 2
 
-      expect(returned_json["user"].length).to be 11
-      expect(returned_json["user"]["rounds"].length).to be 2
+      expect(returned_json[0]["score"]).to eq 76
+      expect(returned_json[0]["birdies"]).to eq 2
+      expect(returned_json[0]["pars"]).to eq 4
+      expect(returned_json[0]["date"]).to eq "07/01/2019"
+      expect(returned_json[0]["course"]).to eq "Boston Golf Course"
+      expect(returned_json[0]["user_id"]).to eq 1
 
-      expect(returned_json["user"]["rounds"][0]["score"]).to eq 76
-      expect(returned_json["user"]["rounds"][0]["birdies"]).to eq 2
-      expect(returned_json["user"]["rounds"][0]["pars"]).to eq 4
-      expect(returned_json["user"]["rounds"][0]["date"]).to eq "07/01/2019"
-      expect(returned_json["user"]["rounds"][0]["course"]).to eq "Boston Golf Course"
-      expect(returned_json["user"]["rounds"][0]["user_id"]).to eq 1
-
-      expect(returned_json["user"]["rounds"][1]["score"]).to eq 80
-      expect(returned_json["user"]["rounds"][1]["birdies"]).to eq 0
-      expect(returned_json["user"]["rounds"][1]["pars"]).to eq 5
-      expect(returned_json["user"]["rounds"][1]["date"]).to eq "06/12/2019"
-      expect(returned_json["user"]["rounds"][1]["course"]).to eq "Mt Hood Municipal Golf Course"
-      expect(returned_json["user"]["rounds"][1]["user_id"]).to eq 1
+      expect(returned_json[1]["score"]).to eq 80
+      expect(returned_json[1]["birdies"]).to eq 0
+      expect(returned_json[1]["pars"]).to eq 5
+      expect(returned_json[1]["date"]).to eq "06/12/2019"
+      expect(returned_json[1]["course"]).to eq "Mt Hood Municipal Golf Course"
+      expect(returned_json[1]["user_id"]).to eq 1
     end
   end
 end
