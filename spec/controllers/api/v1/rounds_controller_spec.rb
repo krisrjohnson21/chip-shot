09b9a115ce02ce313fe4b5425e50fae029d917a5
Kris Johnson <krisrjohnson@gmail.com>
2020-02-21 14:42:55 -0500

    Updated rounds controller test to account for resorted json response

--- spec/controllers/api/v1/rounds_controller_spec.rb
+++ spec/controllers/api/v1/rounds_controller_spec.rb
@@ -37,7 +37,7 @@ RSpec.describe Api::V1::RoundsController, type: :controller do
         user_id: first_user.id,
         rounds: first_user.rounds
       }
-      
+
       returned_json = JSON.parse(response.body)
 
       expect(response.status).to eq 200
@@ -45,18 +45,18 @@ RSpec.describe Api::V1::RoundsController, type: :controller do
 
       expect(returned_json.length).to be 2
 
-      expect(returned_json[0]["score"]).to eq 76
-      expect(returned_json[0]["birdies"]).to eq 2
-      expect(returned_json[0]["pars"]).to eq 4
-      expect(returned_json[0]["date"]).to eq "07/01/2019"
-      expect(returned_json[0]["course"]).to eq "Boston Golf Course"
+      expect(returned_json[0]["score"]).to eq 80
+      expect(returned_json[0]["birdies"]).to eq 0
+      expect(returned_json[0]["pars"]).to eq 5
+      expect(returned_json[0]["date"]).to eq "06/12/2019"
+      expect(returned_json[0]["course"]).to eq "Mt Hood Municipal Golf Course"
       expect(returned_json[0]["user_id"]).to eq 1
 
-      expect(returned_json[1]["score"]).to eq 80
-      expect(returned_json[1]["birdies"]).to eq 0
-      expect(returned_json[1]["pars"]).to eq 5
-      expect(returned_json[1]["date"]).to eq "06/12/2019"
-      expect(returned_json[1]["course"]).to eq "Mt Hood Municipal Golf Course"
+      expect(returned_json[1]["score"]).to eq 76
+      expect(returned_json[1]["birdies"]).to eq 2
+      expect(returned_json[1]["pars"]).to eq 4
+      expect(returned_json[1]["date"]).to eq "07/01/2019"
+      expect(returned_json[1]["course"]).to eq "Boston Golf Course"
       expect(returned_json[1]["user_id"]).to eq 1
     end
   end
