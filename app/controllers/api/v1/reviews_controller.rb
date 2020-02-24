4c7a6e522d1f16cd5db759c1811f4f72b334083f
Kris Johnson <krisrjohnson@gmail.com>
2020-02-23 19:14:02 -0500

    Strengthened authentication on all controllers

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::ReviewsController < ApiController
-  before_action :authenticate_user!, except: [:show, :index]
+  before_action :authenticate_user!, except: [:show]
 
   def index
     render json: Review.all
